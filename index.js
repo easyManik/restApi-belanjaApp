c5da10fbdb8451eee213b6519458dec978efd068
easyManik <easymanik151199@gmail.com>
2023-01-29 21:28:15 +0700

    deployment

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const path = require('path');
 const mainRouter = require('./src/routes');
 
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 4200;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
