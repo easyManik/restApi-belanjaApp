da80ed53a84cee672c12abb336385c4ea7c91069
Easy Destini Manik <72480830+easyManik@users.noreply.github.com>
2023-01-30 11:04:03 +0700

    Update db.js

--- src/config/db.js
+++ src/config/db.js
@@ -5,9 +5,18 @@ const pool = new Pool({
   database: process.env.DB_DATABASE,
   password: process.env.DB_PASSWORD,
   port: process.env.DB_PORT,
+  DB_URI : process.env.DB_URI,
   // ssl: {
   //   rejectUnauthorized: false,
   // },
+});
+
+pool.connect((error, client) => {
+  if (error) {
+    console.log("there is error: ", error)
+  } else {
+    console.log("connect database " +client.database + " in "+client.host)
+  }
 });
 
 module.exports = pool;
